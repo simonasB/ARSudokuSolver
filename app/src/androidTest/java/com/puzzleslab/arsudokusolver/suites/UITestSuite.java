5c7875d80dab5033859cf1fc11c663e6b582a19c
simonasb <baltulionis.simonas@gmail.com>
2016-05-22 18:29:05 +0300

    Added ui test suite

---
+++ app/src/androidTest/java/com/puzzleslab/arsudokusolver/suites/UITestSuite.java
@@ -0,0 +1,14 @@
+package com.puzzleslab.arsudokusolver.suites;
+
+import com.puzzleslab.arsudokusolver.MainActivityTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Simonas on 2016-05-22.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MainActivityTest.class})
+public class UITestSuite {
+}
